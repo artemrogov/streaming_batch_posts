cd63f08646ab551f03406c52baea46fde491c666
artem rogov <artemrogov281@gmail.com>
2023-05-31 01:24:03 +0300

    fix posts

---
+++ src/main/java/com/artemrogov/streaming/config/TemplaterConfig.java
@@ -0,0 +1,30 @@
+package com.artemrogov.streaming.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
+
+@Configuration
+public class TemplaterConfig {
+    @Bean
+    public ClassLoaderTemplateResolver templateConfig() {
+        ClassLoaderTemplateResolver template = new ClassLoaderTemplateResolver();
+        template.setPrefix("templates/");
+        template.setSuffix(".html");
+        template.setTemplateMode(TemplateMode.HTML);
+        template.setCharacterEncoding("UTF-8");
+        template.setOrder(1);
+        template.setCheckExistence(true);
+        return template;
+    }
+
+    @Bean
+    public ResourceBundleMessageSource messageSource() {
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setBasename("messages");
+        return messageSource;
+    }
+}
---
+++ src/main/java/com/artemrogov/streaming/dto/PostResponse.java
@@ -0,0 +1,21 @@
+package com.artemrogov.streaming.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+@Builder
+public class PostResponse implements Serializable {
+    private Long id;
+    private String title;
+    private String content;
+
+}
