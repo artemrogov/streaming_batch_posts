02db030b2d28cd64a5a4a6105c6298fb0d93d93d
artem rogov <artemrogov281@gmail.com>
2023-10-13 01:13:48 +0300

    fix formating and add test

--- src/test/java/com/artemrogov/streaming/DaoDomainTest.java
+++ src/test/java/com/artemrogov/streaming/DaoDomainTest.java
@@ -13,6 +13,7 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
+
 import javax.persistence.TypedQuery;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -31,7 +32,6 @@ public class DaoDomainTest {
     private final static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>(imageName);
 
     private Long postId;
-
     private Long categoryId;
 
     @DynamicPropertySource
@@ -51,64 +51,70 @@ public class DaoDomainTest {
         postgres.stop();
     }
 
-    private List<Post> getMockPostsObjects(){
+    private List<Post> getMockPostsObjects() {
         return Stream.generate(PostEntityGenerator::generate)
                 .limit(5)
                 .collect(Collectors.toList());
     }
 
 
-    private List<Category> getMockCategoriesObjects(){
+    private List<Category> getMockCategoriesObjects() {
         return Stream.generate(CategoryEntityGenerator::generate)
                 .limit(5)
                 .collect(Collectors.toList());
     }
 
-
-
     @BeforeEach
     void setUp() {
-        getMockCategoriesObjects().forEach(category->testEntityManager.persistAndFlush(category));
-        getMockPostsObjects().forEach(post ->testEntityManager.persistAndFlush(post));
-
-        TypedQuery<Post>postsTypedQuery = testEntityManager.getEntityManager().createQuery("from Post",Post.class).setMaxResults(1);
-        TypedQuery<Category> categoryTypedQuery = testEntityManager.getEntityManager().createQuery("from Category", Category.class).setMaxResults(1);
-
+        getMockCategoriesObjects().forEach(category -> testEntityManager.persistAndFlush(category));
+        getMockPostsObjects().forEach(post -> testEntityManager.persistAndFlush(post));
+        TypedQuery<Post> postsTypedQuery = testEntityManager.getEntityManager()
+                .createQuery("from Post", Post.class).setMaxResults(1);
+        TypedQuery<Category> categoryTypedQuery = testEntityManager.getEntityManager()
+                .createQuery("from Category", Category.class).setMaxResults(1);
         Category category = categoryTypedQuery.getSingleResult();
         Post post = postsTypedQuery.getSingleResult();
-
         this.postId = post.getId();
         this.categoryId = category.getId();
-
-        log.info("post_id {}",postId);
-        log.info("category_id {}",categoryId);
     }
 
     @Test
-    public void testCreate(){
-        Post post = new Post();
-        post.setTitle("Test 01");
-        post.setContent("test post");
-        post.setActive(true);
+    public void testCreate() {
+        Post post = PostEntityGenerator.generate();
         testEntityManager.persist(post);
         Assertions.assertNotNull(post.getId());
     }
 
     @Test
-    public void test_with_add_relations(){
-        Post post = testEntityManager.getEntityManager().getReference(Post.class,postId);
-        Category category = testEntityManager.getEntityManager().getReference(Category.class,categoryId);
+    public void testDetach(){
+        Post post = testEntityManager.find(Post.class,postId);
+        testEntityManager.detach(post);
 
+        Assertions.assertNotNull(post);
+
+        Assertions.assertFalse(testEntityManager.getEntityManager().contains(post));
+    }
+
+    @Test
+    public void testMergedDetach(){
+        Post post = testEntityManager.find(Post.class,postId);
+        testEntityManager.detach(post);
+
+        post.setActive(false);
+        Post postUpdated = testEntityManager.merge(post);
+
+        Assertions.assertFalse(postUpdated.getActive());
+    }
+
+
+    @Test
+    public void testMerge() {
+        Post post = testEntityManager.getEntityManager().getReference(Post.class, postId);
+        Category category = testEntityManager.getEntityManager().getReference(Category.class, categoryId);
         post.getCategories().add(category);
         category.getPosts().add(post);
-
-         testEntityManager.merge(category);
-         testEntityManager.flush();
-
+        testEntityManager.merge(category);
+        testEntityManager.flush();
         Assertions.assertNotNull(category.getPosts());
-        category.getPosts().forEach(p->{
-            log.info("id post: "+p.getId());
-            log.info("title post: "+p.getTitle());
-        });
     }
 }
