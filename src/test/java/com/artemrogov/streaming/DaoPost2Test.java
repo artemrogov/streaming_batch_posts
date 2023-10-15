b63b2ab7383f9a3e9b24ac5d47e7a68e1b2031c9
artem rogov <artemrogov281@gmail.com>
2023-10-15 23:52:10 +0300

    reconfigure database test containers

---
+++ src/test/java/com/artemrogov/streaming/DaoPost2Test.java
@@ -0,0 +1,27 @@
+package com.artemrogov.streaming;
+
+
+import com.artemrogov.streaming.dao.PostDao;
+import com.artemrogov.streaming.dao.impl.PostDaoImpl;
+import com.artemrogov.streaming.domain.Post;
+import com.artemrogov.streaming.utils.PostEntityGenerator;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Import;
+
+@Slf4j
+@Import(PostDaoImpl.class)
+public class DaoPost2Test extends PostgresqlTestContainerConfig{
+
+    @Autowired
+    private PostDao postDao;
+
+    @Test
+    public void createPostTest(){
+        Post post = PostEntityGenerator.generate();
+        Post postCreated = postDao.create(post);
+        Assertions.assertNotNull(postCreated);
+    }
+}
---
+++ src/test/java/com/artemrogov/streaming/PostgresqlTestContainerConfig.java
@@ -0,0 +1,34 @@
+package com.artemrogov.streaming;
+
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+public abstract class PostgresqlTestContainerConfig {
+    private final static String imageName = "postgres:13-alpine";
+    private final static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>(imageName);
+
+    @DynamicPropertySource
+    static void configureProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgres::getJdbcUrl);
+        registry.add("spring.datasource.username", postgres::getUsername);
+        registry.add("spring.datasource.password", postgres::getPassword);
+    }
+
+    @BeforeAll
+    static void beforeAll() {
+        postgres.start();
+    }
+
+    @AfterAll
+    static void afterAll() {
+        postgres.stop();
+    }
+}
